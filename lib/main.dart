d667d9a51e8a794ee3836e78189b505e0fb4c57b
Sanduni  Gamage <it19123578@my.sliit.lk>
2022-04-07 23:30:46 +0530

    initial commit2

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,7 @@
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:cricketapp/pages/home.dart';
+import 'package:cricketapp/homescreen/dash_screen.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
@@ -22,5 +23,16 @@ class CricketApp extends StatelessWidget {
       ),
       home: const HomePage(),
     );
+  }
+}
+class HexColor extends Color {
+  HexColor(final String hexColor) : super(_getColorFromHex(hexColor));
+
+  static int _getColorFromHex(String hexColor) {
+    hexColor = hexColor.toUpperCase().replaceAll('#', '');
+    if (hexColor.length == 6) {
+      hexColor = 'FF' + hexColor;
+    }
+    return int.parse(hexColor, radix: 16);
   }
 }
