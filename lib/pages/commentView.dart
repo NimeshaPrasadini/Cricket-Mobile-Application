1cbb4d02b135c07e8fd1b45c3ef4b7798b867276
Nimesha Prasadini <it19028774@my.sliit.lk>
2022-04-09 03:17:05 +0530

    updated image upload for news

--- lib/pages/commentView.dart
+++ lib/pages/commentView.dart
@@ -1,13 +1,116 @@
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter/material.dart';
+//import 'package:flutter/foundation.dart';
+import 'dart:io';
 
-class CommentView extends StatelessWidget {
-  //final Text name;
-  CommentView({Key? key}) : super(key: key);
+class CommentView extends StatefulWidget {
+  const CommentView({Key? key}) : super(key: key);
 
-  //final CollectionReference _comments =
-  //FirebaseFirestore.instance.collection('comments');
-  final db = FirebaseFirestore.instance;
+  @override
+  _CommentViewState createState() => _CommentViewState();
+}
+
+class _CommentViewState extends State<CommentView> {
+
+  // text fields' controllers
+  final TextEditingController _messageController = TextEditingController();
+  final TextEditingController _usernameController = TextEditingController();
+
+
+  final CollectionReference _newss =
+      FirebaseFirestore.instance.collection('comments');
+
+  // This function is triggered when the floatting button or one of the edit buttons is pressed
+  // Adding a news if no documentSnapshot is passed
+  // If documentSnapshot != null then update an existing comment
+  Future<void> _createOrUpdate([DocumentSnapshot? documentSnapshot]) async {
+    String action = 'create';
+    if (documentSnapshot != null) {
+      action = 'update';
+      _messageController.text = documentSnapshot['message'];
+      _usernameController.text = documentSnapshot['userName'];
+    }
+
+    await showModalBottomSheet(
+        isScrollControlled: true,
+        context: context,
+        builder: (BuildContext ctx) {
+          return Padding(
+            padding: EdgeInsets.only(
+                top: 20,
+                left: 20,
+                right: 20,
+                // prevent the soft keyboard from covering text fields
+                bottom: MediaQuery.of(ctx).viewInsets.bottom + 20),
+            child: Column(
+              mainAxisSize: MainAxisSize.min,
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                TextField(
+                  controller: _usernameController,
+                  decoration: const InputDecoration(
+                    labelText: 'User Name',
+                    hintText: 'Enter your name',
+                   filled: true,  
+                  ),   
+                ),
+                TextField(
+                  keyboardType:
+                      TextInputType.multiline,
+                  controller: _messageController,
+                  decoration: const InputDecoration(
+                    labelText: 'Comment',
+                    hintText: 'Enter your comment',
+                   filled: true,
+                  ),
+                  maxLines: 5,
+                  maxLength: 4096,
+                  textInputAction: TextInputAction.done,
+                ),
+                const SizedBox(
+                  height: 20,
+                ),
+                ElevatedButton(
+                  child: Text(action == 'create' ? 'Post' : 'Update'),
+                  onPressed: () async {
+                    final String? message = _messageController.text;
+                    final String? userName = _usernameController.text;
+                    if (message != null && userName != null) {
+                      if (action == 'create') {
+                        // Persist a new comment to Firestore
+                        await _newss.add({"message": message, "userName": userName});
+                      }
+
+                      if (action == 'update') {
+                        // Update the comment
+                        await _newss
+                            .doc(documentSnapshot!.id)
+                            .update({"message": message, "userName": userName});
+                      }
+
+                      // Clear the text fields
+                      _messageController.text = '';
+                      _usernameController.text = '';
+
+                      // Hide the bottom sheet
+                      Navigator.of(context).pop();
+                    }
+                  },
+                )
+              ],
+            ),
+          );
+        });
+  }
+
+  // Deleteing a comment by id
+  Future<void> _deleteNews(String newsId) async {
+    await _newss.doc(newsId).delete();
+
+    // Show a snackbar
+    ScaffoldMessenger.of(context).showSnackBar(const SnackBar(
+        content: Text('You have successfully deleted the comment')));
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -15,143 +118,77 @@ class CommentView extends StatelessWidget {
       appBar: AppBar(
         title: const Text('Comments'),
       ),
-      body: StreamBuilder<QuerySnapshot>(
-        stream: db.collection('comments').snapshots(),
-        builder: (context, snapshot) {
-          if (!snapshot.hasData) {
-            return Center(
-              child: CircularProgressIndicator(),
-            );
-          } else
-            return ListView(
-              children: snapshot.data!.docs.map((doc) {
-                final Text message =
-                            Text((doc.data() as dynamic)['message']);
-                //final Text time = Text(documentSnapshot2['time']);
+      // Using StreamBuilder to display all comments from Firestore in real-time
+      body: StreamBuilder(
+        stream: _newss.snapshots(),
+        builder: (context, AsyncSnapshot<QuerySnapshot> streamSnapshot) {
+          if (streamSnapshot.hasData) {
+            return ListView.builder(
+              itemCount: streamSnapshot.data!.docs.length,
+              itemBuilder: (context, index) {
+                final DocumentSnapshot documentSnapshot =
+                    streamSnapshot.data!.docs[index];
+                bool isFavourite = true;
                 return Card(
+                  margin: const EdgeInsets.all(10),
+                  elevation: 10,
                   child: ListTile(
-                    title: message,
-                    subtitle: Text((doc.data() as dynamic)['name']),
-                    //: Text((doc.data() as dynamic)['time']),
+                    leading: const CircleAvatar(
+                      backgroundColor: Colors.grey,
+                      child: Icon(
+                        Icons.person,
+                        color: Colors.white,
+                      ),
+                    ),
+                    subtitle: Text(documentSnapshot['userName']),
+                    title: Text(documentSnapshot['message']),
+                    trailing: SizedBox(
+                      width: 100,
+                      child: Row(
+                        children: [
+                          // Press this button to edit a single comment
+                          IconButton(
+                              icon: const Icon(Icons.edit),
+                              onPressed: () =>
+                                  _createOrUpdate(documentSnapshot)),
+                          // This icon button is used to delete a single comment
+                          // IconButton(
+                          //     icon: const Icon(Icons.delete),
+                          //     onPressed: () =>
+                          //         _deleteNews(documentSnapshot.id)),                     
+                          IconButton(
+                            icon: Icon(
+                              Icons.favorite,
+                              color: isFavourite ? Colors.blueGrey : Colors.grey
+                            ),
+                            onPressed: ()
+                            {
+                                setState(()
+                                {
+                                  isFavourite = !isFavourite;
+                                });
+                            }
+                          ),
+
+                        ],
+                      ),
+                    ),
                   ),
                 );
-              }).toList(),
+              },
             );
+          }
+
+          return const Center(
+            child: CircularProgressIndicator(),
+          );
         },
       ),
-      //),
+      // Add new comment
       floatingActionButton: FloatingActionButton(
-        onPressed: () {
-          showDialog(
-              context: context, builder: (context) => const FeedbackDialog());
-        },
-        backgroundColor: Colors.blue,
+        onPressed: () => _createOrUpdate(),
         child: const Icon(Icons.add),
       ),
-    );
-  }
-}
-
-class FeedbackDialog extends StatefulWidget {
-  const FeedbackDialog({Key? key}) : super(key: key);
-
-  @override
-  State<FeedbackDialog> createState() => _FeedbackDialogState();
-}
-
-class _FeedbackDialogState extends State<FeedbackDialog> {
-  final TextEditingController _controller = TextEditingController();
-  final TextEditingController _usernamecontroller = TextEditingController();
-  final GlobalKey<FormState> _formKey = GlobalKey();
-
-  @override
-  void dispose() {
-    _controller.dispose();
-    _usernamecontroller.dispose();
-    super.dispose();
-  }
-
-  @override
-  Widget build(BuildContext context) {
-    return AlertDialog(
-      content: Form(
-        key: _formKey,
-        child: Column(
-          children: <Widget>[
-            TextFormField(
-              controller: _usernamecontroller,
-              keyboardType: TextInputType.name,
-              decoration: const InputDecoration(
-                hintText: 'Enter user name here',
-                filled: true,
-              ),
-              textInputAction: TextInputAction.done,
-              validator: (String? text) {
-                if (text == null || text.isEmpty) {
-                  return 'Please enter a value';
-                }
-                return null;
-              },
-            ),
-            TextFormField(
-              controller: _controller,
-              keyboardType: TextInputType.multiline,
-              decoration: const InputDecoration(
-                hintText: 'Enter your comment here',
-                filled: true,
-              ),
-              maxLines: 5,
-              maxLength: 4096,
-              textInputAction: TextInputAction.done,
-              validator: (String? text) {
-                if (text == null || text.isEmpty) {
-                  return 'Please enter a value';
-                }
-                return null;
-              },
-            ),
-          ],
-        ),
-      ),
-      actions: [
-        TextButton(
-          child: const Text('Cancel'),
-          onPressed: () => Navigator.pop(context),
-        ),
-        TextButton(
-          child: const Text('Post'),
-          onPressed: () async {
-            // Only if the input form is valid (the user has entered text)
-            if (_formKey.currentState!.validate()) {
-              // We will use this var to show the result
-              // of this operation to the user
-              String message;
-
-              try {
-                // Get a reference to the `comment` collection
-                final collection =
-                    FirebaseFirestore.instance.collection('comments');
-
-                // Write the user's comment
-                await collection.doc().set({
-                  'message': _controller.text,
-                  'user': _usernamecontroller.text,
-                  'time': FieldValue.serverTimestamp(),
-                });
-
-                message = 'Comment added successfully';
-              } catch (e) {
-                message = 'Error when posting comment';
-              }
-
-              // Show a snackbar with the result
-              ScaffoldMessenger.of(context)
-                  .showSnackBar(SnackBar(content: Text(message)));
-              Navigator.pop(context);
-            }
-          },
-        )
-      ],
     );
   }
 }
--- lib/service/storage_service.dart
+++ lib/service/storage_service.dart
@@ -1,5 +1,4 @@
 import 'dart:io';
-
 import 'package:firebase_storage/firebase_storage.dart' as firebase_storage;
 import 'package:firebase_core/firebase_core.dart' as firebase_core;
 
@@ -35,6 +34,38 @@ class Storage {
   ) async {
     String downloadURL =
         await storage.ref('Players/$imageName').getDownloadURL();
+    return downloadURL;
+  }
+
+  //News Image Upload
+  Future<void> uploadNewsImage(
+    String filePath,
+    String fileName,
+  ) async {
+    File file = File(filePath);
+
+    try {
+      await storage.ref('News/$fileName').putFile(file);
+    } on firebase_core.FirebaseException catch (e) {
+      print(e);
+    }
+  }
+
+  Future<firebase_storage.ListResult> listNewsFiles() async {
+    firebase_storage.ListResult results =
+        await storage.ref('News').listAll();
+
+    results.items.forEach((firebase_storage.Reference ref) {
+      print('Found files: $ref');
+    });
+    return results;
+  }
+
+  Future<String> downloadNewsURL(
+    String imageName,
+  ) async {
+    String downloadURL =
+        await storage.ref('News/$imageName').getDownloadURL();
     return downloadURL;
   }
 }
